fe6ac0239e138da5d655e371e29999de4944659f
NidhishKR <nidhish@qburst.com>
2017-05-29 13:05:48 +0530

    Update import.sh

--- import.sh
+++ import.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-aws s3 cp s3://<bucket name>/ / --recursive --exclude "*" --include "*<dump file name>*" && mongoimport --host mongodb --db newdb --collection restaurants --file primer-dataset.json
+aws s3 cp s3://<bucket name>/ / --recursive --exclude "*" --include "*<dump file name>*" && mongoimport --host mongodb --db <DB name> --collection <collection name> --file <dump file name>
